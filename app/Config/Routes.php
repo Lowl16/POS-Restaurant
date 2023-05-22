a7217b9bbb856d4c6fe1e7e96f80317021bf80b6
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2023-05-22 08:31:56 +0700

    update cashier routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -29,12 +29,16 @@ $routes->set404Override();
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
+
+$routes->get('/dashboard', 'Dashboard::index');
+
+$routes->get('/customer', 'Customer::index');
 $routes->get('/customer/(:any)', 'Customer::$1');
 $routes->post('/customer/(:any)', 'Customer::$1');
 
-$routes->get('/dashboard', 'Dashboard::index');
-$routes->get('/customer', 'Customer::index');
-$routes->get('/chart', 'Chart::index');
+$routes->get('/cashier', 'Cashier::index');
+$routes->get('/cashier/(:any)', 'Cashier::$1');
+$routes->post('/cashier/(:any)', 'Cashier::$1');
 
 $routes->get('/', 'Main::index');
 
