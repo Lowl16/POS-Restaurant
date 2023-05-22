0a8c20331ea981f210eca992cdce751683edc1a0
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2023-05-22 08:34:22 +0700

    update view dashboard

--- app/Views/admin/dashboard.php
+++ app/Views/admin/dashboard.php
@@ -7,14 +7,14 @@
             <!-- small box -->
             <div class="small-box bg-info">
               <div class="inner">
-                <h3>150</h3>
+                <h3><?php echo $customerRowCount ?></h3>
 
                 <p>Customers</p>
               </div>
               <div class="icon">
                 <i class="ion ion-bag"></i>
               </div>
-              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+              <a href="<?= base_url('customer')?>" class="small-box-footer">More info ... <i class="fas fa-arrow-circle-right"></i></a>
             </div>
           </div>
           <!-- ./col -->
