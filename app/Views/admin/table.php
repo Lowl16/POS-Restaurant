e7f3c46ab8a95da05fc53ec0b6725563d8f8138d
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2023-05-25 23:25:17 +0700

    Create table.php

---
+++ app/Views/admin/table.php
@@ -0,0 +1,76 @@
+<!-- Main content -->
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+
+<!-- Font Awesome -->
+<link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+
+<!-- DataTables -->
+<link rel="stylesheet" href="plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+<link rel="stylesheet" href="plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+<link rel="stylesheet" href="plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+
+<!-- Theme style -->
+<link rel="stylesheet" href="dist/css/adminlte.min.css">
+
+<style>
+    .table-responsive {
+    overflow-x: auto;
+    }
+</style>
+
+<section class="content">
+    <div class="container-fluid">
+    <div class="row">
+        <div class="col-12">
+        <div class="card">
+            <div class="card-header">
+            <h3 class="card-title">List of Table</h3>
+            </div>
+            <!-- /.card-header -->
+
+            <div class="card-body">
+            <a class="btn btn-success mb-3" href="<?= base_url('table/create')?>"><i class="fa-solid fa-plus"></i> Add Table</a>
+            <div class="table-responsive">
+            <table id="example2" class="table table-striped table-hover">
+                <colgroup>
+                <col width="5%">
+                <col width="30%">
+                <col width="25%">
+                <col width="30%">
+                </colgroup>
+                <thead>
+                <tr class="bg-gradient bg-dark text-light">
+                    <th class="py-1 text-center">#</th>
+                    <th class="py-1 text-center">Name</th>
+                    <th class="py-1 text-center">Size</th>
+                    <th class="py-1 text-center">Action</th>
+                </tr>
+                </thead>
+                <tbody>
+                <?php if(count($list) > 0): ?>
+                    <?php $i = 1; ?>
+                    <?php foreach($list as $row): ?>
+                        <tr>
+                            <th class="p-1 align-middle text-center"><?= $i++ ?></th>
+                            <td class="p-1 align-middle text-center"><?= $row->name ?></td>
+                            <td class="p-1 align-middle text-center"><?= $row->size ?></td>
+                            <td class="p-1 align-middle text-center">
+                                <div class="btn-group btn-group-sm">
+                                    <a href="<?= base_url('table/edit/'.$row->id) ?>" class="btn btn-warning rounded-left" title="Edit Table"><i class="fa fa-edit"></i> Update</a>
+                                    <a href="<?= base_url('table/delete/'.$row->id) ?>" onclick="if(confirm('Are you sure to delete this table details?') === false) event.preventDefault()" class="btn btn-danger rounded-right" title="Delete Table"><i class="fa fa-trash"></i> Delete</a>
+                                </div>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                <?php endif; ?>
+                </tbody>
+            </table>
+            </div>
+            </div>
+            <!-- /.card-body -->
+
+        </div>
+        </div>
+    </div>
+</div>
+</section>
