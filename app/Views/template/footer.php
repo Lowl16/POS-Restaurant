d990655416517823426e6ae05e658132657d4e6a
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2023-05-25 23:25:29 +0700

    Update footer.php

--- app/Views/template/footer.php
+++ app/Views/template/footer.php
@@ -1,5 +1,5 @@
 <footer class="footer footer-center p-4 bg-base-100 text-base-content">
-  <div>
-    <p>Copyright © 2023 - All right reserved by ACME Industries Ltd</p>
-  </div>
+    <div>
+        <p>Copyright © 2023 - All right reserved by ACME Industries Ltd</p>
+    </div>
 </footer>
