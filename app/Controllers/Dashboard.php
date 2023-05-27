2d52ae6b27793081730eb65628795a021ff6ac2c
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2023-05-27 21:36:39 +0700

    Update Dashboard.php

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -2,8 +2,10 @@
 
 namespace App\Controllers;
 
+use App\Models\OrdersModel;
 use App\Models\ProductsModel;
 use App\Models\UsersModel;
+use App\Models\PaymentsModel;
 
 class Dashboard extends BaseController
 {
@@ -16,10 +18,14 @@ class Dashboard extends BaseController
     // Model
     protected $customer_model;
     protected $product_model;
+    protected $order_model;
+    protected $payment_model;
 
     public function __construct(){
         $this->customer_model = new UsersModel();
         $this->product_model = new ProductsModel();
+        $this->order_model = new OrdersModel();
+        $this->payment_model = new PaymentsModel();
         $this->session= \Config\Services::session();
         $this->data['session'] = $this->session;
     }
@@ -30,6 +36,8 @@ class Dashboard extends BaseController
         $this->data['activeMenu'] = "dashboard";
         $this->data['customerRowCount'] = $this->customer_model->where('role', 'customer')->countAllResults();
         $this->data['productRowCount'] = $this->product_model->countAllResults();
+        $this->data['orderRowCount'] = $this->order_model->countAllResults();
+        $this->data['salesCount'] = $this->payment_model->selectSum('pay_amount')->get()->getRow();
 
         echo view('admin/header', $this->data);
         echo view('admin/dashboard', $this->data);
