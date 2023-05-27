5cb3fdcc91dfe8a05e758edb86e609125bd92a72
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2023-05-27 21:37:41 +0700

    Update footer.php

--- app/Views/admin/footer.php
+++ app/Views/admin/footer.php
@@ -3,10 +3,9 @@
 
 <!-- /.content-wrapper -->
 <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
-    All rights reserved.
+    <strong>Copyright © 2023 - Developed by <a href="https://github.com/Lowl16">Sandhi Karunia Sugihartana</a></strong>
     <div class="float-right d-none d-sm-inline-block">
-    <b>Version</b> 3.2.0
+    <b>Credit to <a href="https://adminlte.io">AdminLTE</a> Version 3.2.0</b>
     </div>
 </footer>
 
