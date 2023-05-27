6d70da889f942e1a3e8ea2926bb13c4c182f0f6b
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2023-05-27 21:37:51 +0700

    Update order.php

--- app/Views/admin/order.php
+++ app/Views/admin/order.php
@@ -30,21 +30,14 @@
 
             <div class="card-body">
             <div class="table-responsive">
-            <table id="example2" class="table table-striped table-hover">
-                <colgroup>
-                <col width="5%">
-                <col width="35%">
-                <col width="10%">
-                <col width="35%">
-                <col width="15%">
-                </colgroup>
+            <table id="order" class="table table-striped table-hover">
                 <thead>
                 <tr class="bg-gradient bg-dark text-light">
-                    <th class="py-1 text-center">#</th>
-                    <th class="py-1 text-center">Name</th>
-                    <th class="py-1 text-center">Price</th>
-                    <th class="py-1 text-center">Image</th>
-                    <th class="py-1 text-center">Action</th>
+                    <th class="text-center">#</th>
+                    <th class="text-center">Name</th>
+                    <th class="text-center">Price</th>
+                    <th class="text-center">Image</th>
+                    <th class="text-center">Action</th>
                 </tr>
                 </thead>
                 <tbody>
@@ -75,3 +68,12 @@
     </div>
 </div>
 </section>
+
+<script>
+  $(function () {
+    $("#order").DataTable({
+      "responsive": true, "lengthChange": false, "autoWidth": false,
+      "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+    }).buttons().container().appendTo('#order_wrapper .col-md-6:eq(0)');
+  });
+</script>
