f56934788946edd4f5dc7ec04edff98c047da433
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2023-05-27 21:37:17 +0700

    Update cashier.php

--- app/Views/admin/cashier.php
+++ app/Views/admin/cashier.php
@@ -30,20 +30,13 @@
 
             <div class="card-body">
             <a class="btn btn-success mb-3" href="<?= base_url('cashier/create')?>"><i class="fa-solid fa-plus"></i> Add Cashier</a>
-            <div class="table-responsive">
-            <table class="table table-striped table-hover">
-                <colgroup>
-                <col width="5%">
-                <col width="30%">
-                <col width="25%">
-                <col width="30%">
-                </colgroup>
+            <table id="cashier" class="table table-striped table-hover">
                 <thead>
                 <tr class="bg-gradient bg-dark text-light">
-                    <th class="py-1 text-center">#</th>
-                    <th class="py-1 text-center">Username</th>
-                    <th class="py-1 text-center">Email</th>
-                    <th class="py-1 text-center">Action</th>
+                    <th class="text-center">#</th>
+                    <th class="text-center">Username</th>
+                    <th class="text-center">Email</th>
+                    <th class="text-center">Action</th>
                 </tr>
                 </thead>
                 <tbody>
@@ -74,3 +67,12 @@
     </div>
 </div>
 </section>
+
+<script>
+  $(function () {
+    $("#cashier").DataTable({
+      "responsive": true, "lengthChange": false, "autoWidth": false,
+      "buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+    }).buttons().container().appendTo('#cashier_wrapper .col-md-6:eq(0)');
+  });
+</script>
