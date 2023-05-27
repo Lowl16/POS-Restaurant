f0c5c54ee0c9d6242499a45a26f2c49569c68f3a
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2023-05-27 21:38:20 +0700

    Update login.php

--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -12,8 +12,16 @@
 	<div class="flex items-center justify-center min-h-screen bg-gray-100">
       	<div class="relative flex flex-col m-6 space-y-8 bg-white shadow-2xl rounded-2xl md:flex-row md:space-y-0">
 
-			<!-- left side -->
+            <!-- left side -->
+			<div class="relative">
+				<img src="img/chef.png" alt="img" class="w-[400px] h-full hidden rounded-r-2xl md:block object-cover transform -scale-x-100"/>
+			</div>
+
+			<!-- right side -->
 			<div class="flex flex-col justify-center p-8 md:p-14">
+                <a class="flex items-center text-xl mb-7" style="color: #0d6efd;" href="<?= base_url() ?>">
+                    <i class="fa-solid fa-angle-left mr-2"></i> Back to Home
+                </a>
 				<span class="mb-3 text-4xl font-bold">Login</span>
 				<span class="font-light text-black mb-4">
 					Please enter your information
@@ -42,7 +50,6 @@
 							<input type="checkbox" name="remember" id="check" />
 							<label for="check">Remember me</label>
 						</div>
-						<a href="">Forgot password?</a>
 					</div>
 					<button class="w-full bg-blue-500 text-white p-2 mt-3 rounded-lg mb-6 border hover:bg-secondary hover:text-primary hover:border-gray-300">Sign in</button>
 				</form>
@@ -50,11 +57,6 @@
 				<div class="text-center">
 					Don't have an account? <a href="<?php echo base_url('register'); ?>" class="text-blue-500 underline">Sign up for free</a>
 				</div>
-			</div>
-
-			<!-- right side -->
-			<div class="relative">
-				<img src="img/chef.png" alt="img" class="w-[400px] h-full hidden rounded-r-2xl md:block object-cover"/>
 			</div>
 		</div>
 	</div>
