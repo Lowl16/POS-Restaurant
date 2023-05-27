d8a883c816f0eecf54ce334669f6cb93eeb3c915
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2023-05-27 21:38:33 +0700

    Update home.php

--- app/Views/user/home.php
+++ app/Views/user/home.php
@@ -1,63 +1,14 @@
 <div class="hero min-h-screen bg-base-200">
     <div class="hero-content flex-col lg:flex-row-reverse">
-        <img src="img/vegan1.png" class="max-w-sm rounded-lg md:ml-20 md:-mt-10" />
+        <img src="img/product1.png" class="max-w-sm rounded-lg md:ml-20 md:-mt-10" />
         <div>
         <h1 class="text-5xl font-bold text-primary">Healthy food to <br> live a healthier life <br> in the future</h1>
         <p class="py-6">Enjoy a healty life by eating healty foods <br> that have extraordinary flavors that make your life healthier <br> for today and in the future</p>
-        <button class="btn bg-slate-800 text-white hover:bg-white hover:text-primary mb-10">Get Started</button>
-        </div>
-    </div>
-</div>
-
-<div class="container mx-auto py-12 mb-6">
-    <h2 class="text-3xl font-bold text-center mb-12">Our Menu</h2>
-    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-        <div class="card shadow-lg bg-base-100">
-        <figure>
-            <img src="img/vegan2.png" alt="Menu 1" class="rounded-t-lg" />
-        </figure>
-        <div class="card-body">
-            <h3 class="card-title font-bold text-xl mb-2">Menu 1</h3>
-            <p class="text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, quidem fugiat. Dolorum recusandae quod ut quidem, officiis reprehenderit animi molestias.</p>
-            <div class="card-actions justify-end mt-4">
-            <button class="btn btn-primary">Order Now</button>
-            </div>
-        </div>
-        </div>
-        <div class="card shadow-lg bg-base-100">
-        <figure>
-            <img src="img/vegan2.png" alt="Menu 2" class="rounded-t-lg" />
-        </figure>
-        <div class="card-body">
-            <h3 class="card-title font-bold text-xl mb-2">Menu 2</h3>
-            <p class="text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, quidem fugiat. Dolorum recusandae quod ut quidem, officiis reprehenderit animi molestias.</p>
-            <div class="card-actions justify-end mt-4">
-            <button class="btn btn-primary">Order Now</button>
-            </div>
-        </div>
-        </div>
-        <div class="card shadow-lg bg-base-100">
-        <figure>
-            <img src="img/vegan2.png" alt="Menu 3" class="rounded-t-lg" />
-        </figure>
-        <div class="card-body">
-            <h3 class="card-title font-bold text-xl mb-2">Menu 3</h3>
-            <p class="text-base">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, quidem fugiat. Dolorum recusandae quod ut quidem, officiis reprehenderit animi molestias.</p>
-            <div class="card-actions justify-end mt-4">
-            <button class="btn btn-primary">Order Now</button>
-            </div>
-        </div>
-        </div>
-    </div>
-</div>
-
-<div class="hero min-h-screen" style="background-image: url(img/food.jpg);">
-    <div class="hero-overlay bg-opacity-60"></div>
-    <div class="hero-content text-center text-neutral-content">
-        <div class="max-w-md">
-        <h1 class="mb-5 text-5xl font-bold">Hello there</h1>
-        <p class="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-        <button class="btn btn-primary">Get Started</button>
+        <?php if($session->has('logged_in') == TRUE){ ?>
+            <a href="<?= base_url()?>dashboard" class="btn bg-slate-800 text-white hover:bg-white hover:text-primary mb-10">Get Started</a>
+        <?php } else { ?>
+            <a href="<?= base_url()?>login" class="btn bg-slate-800 text-white hover:bg-white hover:text-primary mb-10">Get Started</a>
+        <?php } ?>
         </div>
     </div>
 </div>
