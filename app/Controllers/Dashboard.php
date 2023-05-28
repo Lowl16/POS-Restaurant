003dcfd5ea4948ca2ed0cebf6f7d60e9b7568905
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2023-05-28 22:58:14 +0700

    Update Dashboard.php

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -6,6 +6,8 @@ use App\Models\OrdersModel;
 use App\Models\ProductsModel;
 use App\Models\UsersModel;
 use App\Models\PaymentsModel;
+use App\Models\ReportsModel;
+use App\Models\TablesModel;
 
 class Dashboard extends BaseController
 {
@@ -16,16 +18,22 @@ class Dashboard extends BaseController
     protected $data;
 
     // Model
+    protected $cashier_model;
     protected $customer_model;
+    protected $table_model;
     protected $product_model;
     protected $order_model;
     protected $payment_model;
+    protected $report_model;
 
     public function __construct(){
+        $this->cashier_model = new UsersModel();
         $this->customer_model = new UsersModel();
+        $this->table_model = new TablesModel();
         $this->product_model = new ProductsModel();
         $this->order_model = new OrdersModel();
         $this->payment_model = new PaymentsModel();
+        $this->report_model = new ReportsModel();
         $this->session= \Config\Services::session();
         $this->data['session'] = $this->session;
     }
@@ -39,8 +47,33 @@ class Dashboard extends BaseController
         $this->data['orderRowCount'] = $this->order_model->countAllResults();
         $this->data['salesCount'] = $this->payment_model->selectSum('pay_amount')->get()->getRow();
 
+        $this->data['list'] = $this->report_model->select('*')->get()->getResult();
+
         echo view('admin/header', $this->data);
         echo view('admin/dashboard', $this->data);
         echo view('admin/footer');
+    }
+
+    public function generate(){
+        $post = [
+            'cashier_count' => (int) $this->cashier_model->where('role', 'cashier')->countAllResults(),
+            'customer_count' => (int) $this->customer_model->where('role', 'customer')->countAllResults(),
+            'table_count' => (int) $this->table_model->countAllResults(),
+            'product_count' => (int) $this->product_model->countAllResults(),
+            'order_count' => (int) $this->order_model->countAllResults(),
+            'order_paid' => (int) $this->order_model->where('order_status', 'PAID')->countAllResults(),
+            'order_pending' => (int) $this->order_model->where('order_status', 'PENDING')->countAllResults(),
+            'order_unpaid' => (int) $this->order_model->where('order_status', 'UNPAID')->countAllResults(),
+            'total_sales' => (int) $this->payment_model->selectSum('pay_amount')->get()->getRow()->pay_amount
+        ];
+        $save = $this->report_model->insert($post);
+        if($save){
+            $this->session->setFlashdata('success_message','Data has been generated successfully') ;
+            return redirect()->to('/dashboard/index');
+        }else{
+            echo view('admin/header', $this->data);
+            echo view('admin/dashboard', $this->data);
+            echo view('admin/footer');
+        }
     }
 }
