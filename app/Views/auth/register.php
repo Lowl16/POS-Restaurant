149db94efc0bb00ce27160f6149dbf67cd976ad9
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2023-05-28 22:59:13 +0700

    Update register.php

--- app/Views/auth/register.php
+++ app/Views/auth/register.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
     <link rel="stylesheet" href="/css/app.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" /> 
+</head>
 </head>
 <body>
 	<div class="flex items-center justify-center min-h-screen bg-gray-100">
@@ -29,26 +30,26 @@
                 <?php if (!empty(session()->getFlashdata('error'))) : ?>
                     <div role="alert" class="mb-4 mt-4">
                         <div class="bg-red-500 text-white font-bold border-red-400 rounded-t-md px-4 py-2">
-                            Registration failed
+                            Registration failed!
                         </div>
                         <div class="border border-t-0 border-red-400 rounded-b-md bg-red-100 px-4 py-3 text-red-700">
                             <?php echo session()->getFlashdata('error'); ?>
                         </div>
                     </div>
                 <?php endif; ?>
-				<form action="<?= base_url(); ?>/register/process" method="POST">
+				<form action="<?= base_url(); ?>/register/process" method="POST" onsubmit="showErrorL">
                     <?= csrf_field(); ?>
                     <div class="py-4">
 						<span class="mb-2 text-md mr-24">Username</span>
-						<input type="text" class="w-full p-2 border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500" name="username" id="username" required>
+						<input type="text" class="w-full p-2 border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500" name="username" id="username" placeholder="Username" required>
 					</div>
 					<div class="py-4">
 						<span class="mb-2 text-md mr-24">Email</span>
-						<input type="email" class="w-full p-2 border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500" name="email" id="email" required>
+						<input type="email" class="w-full p-2 border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500" name="email" id="email" placeholder="Email" required>
 					</div>
 					<div class="py-4">
 						<span class="mb-2 text-md">Password</span>
-						<input type="password" name="password" id="password" class="w-full p-2 border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500" required>
+						<input type="password" name="password" id="password" class="w-full p-2 border border-gray-300 rounded-md placeholder:font-light placeholder:text-gray-500" placeholder="Password" required>
 					</div>
 					
 					<button type="submit" class="w-full bg-blue-500 text-white p-2 mt-3 rounded-lg mb-6 border hover:bg-secondary hover:text-primary hover:border-gray-300">Sign up</button>
