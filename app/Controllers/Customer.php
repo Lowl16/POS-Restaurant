31749d2a4199c45b73bc5f174073e289fe85e4a1
Sandhi Karunia Sugihartana <sandhi372@gmail.com>
2023-05-28 22:57:45 +0700

    Update Customer.php

--- app/Controllers/Customer.php
+++ app/Controllers/Customer.php
@@ -88,28 +88,56 @@ class Customer extends BaseController
         $this->data['request'] = $this->request;
         $this->data['title'] = "Update Customer";
         $this->data['activeMenu'] = "customer";
-        $role = 'customer';
-        $post = [
-            'username' => $this->request->getPost('username'),
-            'email' => $this->request->getPost('email'),
-            'password' => password_hash($this->request->getVar('password'), PASSWORD_DEFAULT),
-            'role' => $role
-        ];
-        if(!empty($this->request->getPost('id')))
-            $save = $this->customer_model->where(['id'=>$this->request->getPost('id')])->set($post)->update();
-        else
-            $save = $this->customer_model->insert($post);
-        if($save){
+        if (!$this->validate([
+            'username' => [
+                'rules' => 'required|min_length[4]|max_length[16]',
+                'errors' => [
+                    'required' => 'Username is required',
+                    'min_length' => 'Username must be at least 4 characters',
+                    'max_length' => 'Username cannot exceed 16 characters',
+                ]
+            ],
+            'email' => [
+                'rules' => 'required',
+                'errors' => [
+                    'required' => 'Email is required',
+                ]
+            ],
+            'password' => [
+                'rules' => 'required|min_length[4]|max_length[50]',
+                'errors' => [
+                    'required' => 'Password is required',
+                    'min_length' => 'Password must be at least 4 characters',
+                    'max_length' => 'Password cannot exceed 50 characters',
+                ]
+            ],
+        ])) {
+            session()->setFlashdata('error', $this->validator->listErrors());
+            return redirect()->back()->withInput();
+        } else {
+            $role = 'customer';
+            $post = [
+                'username' => $this->request->getPost('username'),
+                'email' => $this->request->getPost('email'),
+                'password' => password_hash($this->request->getVar('password'), PASSWORD_DEFAULT),
+                'role' => $role
+            ];
             if(!empty($this->request->getPost('id')))
-            $this->session->setFlashdata('success_message','Data has been updated successfully') ;
+                $save = $this->customer_model->where(['id'=>$this->request->getPost('id')])->set($post)->update();
             else
-            $this->session->setFlashdata('success_message','Data has been added successfully') ;
-            $id =!empty($this->request->getPost('id')) ? $this->request->getPost('id') : $save;
-            return redirect()->to('/customer/index');
-        }else{
-            echo view('admin/header', $this->data);
-            echo view('admin/createcustomer', $this->data);
-            echo view('admin/footer');
+                $save = $this->customer_model->insert($post);
+            if($save){
+                if(!empty($this->request->getPost('id')))
+                $this->session->setFlashdata('success_message','Data has been updated successfully') ;
+                else
+                $this->session->setFlashdata('success_message','Data has been added successfully') ;
+                $id =!empty($this->request->getPost('id')) ? $this->request->getPost('id') : $save;
+                return redirect()->to('/customer/index');
+            }else{
+                echo view('admin/header', $this->data);
+                echo view('admin/createcustomer', $this->data);
+                echo view('admin/footer');
+            }
         }
     }
 
